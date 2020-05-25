132c2e353c53a82cbc1503ea8a4cab7a5e433145
alice maria spina <alicem.spina93@gmail.com>
2020-05-25 15:46:21 +0200

    commit ore 15;46

--- js/script.js
+++ js/script.js
@@ -2,20 +2,20 @@
 var emailUtente = prompt('Inserisci la tua email');
 
 // stabilisco elenco email di chi può accedere alla pagina
-var emailAccesso = ["alice.spina@gmail.com","guiovanni.silo@gmail.com","erica.pilla@gmail.com","giuseppe.farina@gmail.com","marianna.celle@gmail.com"]
+var emailAccesso = ["alice.spina@gmail.com","guiovanni.silo@gmail.com","erica.pilla@gmail.com","giuseppe.farina@gmail.com","marianna.celle@gmail.com"];
 
 // stabilisco variabile emailUtenteAccesso
 var emailUtenteAccesso = false;
 //controllo che mail utente sia nella lista
-for (var i = 0; i < emailConAccesso.lenght; i++) {
-  if (emailAccesso == emailUtente) {
+for (var i = 0; i < emailAccesso.lenght; i++) {
+  if (emailAccesso[i] == emailUtente) {
     emailUtenteAccesso = true;
   }
 }
 
 // messaggio da dare all'utente in base all'esito del controllo
-if() {
-
-} else {
-
+if(emailUtenteAccesso = true;) {
+  alert("ciao! puoi accedere alla pagina");
+} else (emailUtenteAccesso = false;) {
+  alert("ciao,  per accedere alla pagina registrati o inserisci la mail corretta");
 }
